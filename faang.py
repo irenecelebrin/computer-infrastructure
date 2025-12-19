6ee27dd037cd816eec074d63180b4f67391887c9
Irene Celebrin <irene.celebrin@gmail.com>
2025-12-19 19:29:11 +0000

    add color mapping to faang.py

--- faang.py
+++ faang.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 
 
-# 1. Get data about FAANG stocks and save it 
+# 1. Get data about FAANG stock and save it 
 
 tickers = yf.Tickers('META AAPL AMZN NFLX GOOG')
 
@@ -51,35 +51,61 @@ df_latest_tickers['Close_datetime'] = df_latest_tickers.index.astype(str).str.fi
 eod_tickers = df_latest_tickers[df_latest_tickers['Close_datetime'] != ' ']
 eod_tickers.loc[:,('Date')] = eod_tickers['Date'].replace(to_replace=' .+$', value='', regex=True)
 
-# plot hourly "Close" prices in the past 5 days
+# Plots
+# set labels and colors
 
-# axes 
-close_price = df_latest_tickers['Close']
+# tickers
+tickers = list(df_latest_tickers['Close'])
+# colors
+color_map = {
+    "AAPL": "tab:blue",
+    "AMZN": "tab:orange",
+    "GOOG": "tab:green",
+    "META": "tab:red",
+    "NFLX": "tab:purple",
+}
+
+# Plot hourly Close price in the past 5 days 
+
+# X axis
 date = df_latest_tickers['Date']
+# Y axis
+close_price = df_latest_tickers['Close']
 
-# plot
-fig, ax = plt.subplots(figsize=(16,16))
-ax.plot(date, close_price)
-ax.set_xlabel('Date and Time')
-ax.set_ylabel('Close Price (USD)')
-ax.legend(labels = ["AAPL", "AMZN", "GOOG", "META", "NFLX"], fontsize = 'x-large', loc = 'center right')
+
+# plot the data
+fig, ax = plt.subplots(figsize=(16, 16))
+
+for ticker in tickers:
+    ax.plot(date, close_price[ticker], color = color_map[ticker])
+        
+ax.set_xlabel('Date and Time', fontsize=18)
+ax.set_ylabel('Close Price (USD)', fontsize=18)
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.legend.html
+ax.legend(labels = tickers, fontsize = 'x-large', loc = "center right")
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.xticks.html 
 ax.set_xticks(date, labels = date, rotation = 90) 
-ax.set_title('FAANG Stocks - Close price over last 5 days')
+# title
+ax.set_title('FAANG stock: hourly Close price (last 5d)', fontsize=22, fontweight="bold" )
 
-# save
+# save the plot
 # verify if the folder exists, if not, create it: 
 if not os.path.exists('images/plots'):
     os.makedirs('images/plots') 
 
+# set directory
 plots_folder = 'images/plots/'
 image_name = latest_tickers_data.strip('.csv') + '.png'
+# save 
 plt.savefig(plots_folder + image_name, dpi=100)
+
 
 # plot EOD "Close" prices in subplots 
 
-# create arrays for axes and labels 
+# X axis 
 eod_date = eod_tickers['Date'] 
 
+# Y axis
 aapl = eod_tickers[('Close','AAPL')]
 amzn = eod_tickers[('Close','AMZN')]
 goog = eod_tickers[('Close','GOOG')]
@@ -87,22 +113,30 @@ meta = eod_tickers[('Close','META')]
 nflx = eod_tickers[('Close','NFLX')]
 
 data = [aapl, amzn, goog, meta, nflx]
-titles = ['AAPL', 'AMZN', 'GOOG', 'META', 'NFLX']
 
 
 # plot the data 
 fig, axs = plt.subplots(2, 3, figsize=(14, 8))
+
+# Flatten the 2D array of axes for easy iteration
 axs = axs.flatten()
 
-for i, (ax, series, title) in enumerate(zip(axs, data, titles)):
-    ax.plot(eod_date, series)
-    ax.set_title(title)
+for ax, series, ticker in zip(axs, data, tickers):
+    ax.plot(eod_date, series, color=color_map.get(ticker, "black"))
+    ax.set_title(ticker)
+    ax.set_ylabel("Close price (USD)")
     ax.tick_params(axis='x', rotation=90)
 
+# Hide the last unused subplot (the 6th one)
 axs[-1].set_visible(False)
+
+# add title. see: https://chatgpt.com/s/t_6945a44b5b8c819186585a8c21d20677
+fig.suptitle("FAANG stock: EOD Close price (last 5d)", fontsize=18, fontweight="bold")
+
+# Adjust spacing between plots
 fig.tight_layout(pad=3.0)
 
-# save the plots 
+# save 
 image_name = latest_tickers_data.strip('.csv') + '_subplots.png'
 plt.savefig(plots_folder + image_name)
 
